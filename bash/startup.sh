bd2e048d06a086ac35f6ee06d13ef11275a833b5
Drew <andrewbdow@gmail.com>
2023-03-27 16:33:46 -0500

    * updated prompt and file names to make more sense

--- bash/startup.sh
+++ bash/startup.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 
 # Set the default docker-compose file
-DOCKER_FILE=docker-compose-light.yml
+DOCKER_FILE=cp-light.yml
 
 function spin_up {
     # Ask for the docker-compose file
-    echo "Which docker-compose file do you want to spin up?"
-    select docker_file in "docker-compose-light.yml" "docker-compose-full.yml"; do
+    echo "What kafka instance do you want to spin up?"
+    select docker_file in "cp-light.yml" "cp-full.yml"; do
         case $docker_file in
-            docker-compose-light.yml ) DOCKER_FILE=docker-compose-light.yml; break;;
-            docker-compose-full.yml ) DOCKER_FILE=docker-compose-full.yml; break;;
+            cp-light.yml ) DOCKER_FILE=cp-light.yml; break;;
+            cp-full.yml ) DOCKER_FILE=cp-full.yml; break;;
         esac
     done
+
+if [ $DOCKER_FILE == cp-full.yml ]
+  then
+   echo "This might take awhile creating containers.."
+   sleep 1
+else
+   echo "Shouldn't take long creating containers now.."
+   sleep 1
+fi
 
 # Notify user of selected option
 echo "Selected: $docker_file"
